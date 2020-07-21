2665310c42bad6255dcb7b056f80be737d3b3727
Alexsandro de Lima Silva <alexlima_cti@hotmail.com>
2020-07-21 08:54:39 -0300

    Aula 8

--- Aula 03/TestesUnitarios/src/test/java/com/indeas/servicos/LocacaoServiceTest.java
+++ Aula 03/TestesUnitarios/src/test/java/com/indeas/servicos/LocacaoServiceTest.java
@@ -3,12 +3,13 @@ package com.indeas.servicos;
 import static com.indeas.utils.DataUtils.isMesmaData;
 import static com.indeas.utils.DataUtils.obterDataComDiferencaDias;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 
 import java.util.Date;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import com.indeas.entidades.Filme;
 import com.indeas.entidades.Locacao;
@@ -16,8 +17,11 @@ import com.indeas.entidades.Usuario;
 
 public class LocacaoServiceTest {
 	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
-	public void testes() {
+	public void testesLocacao() {
 		LocacaoService ls = new LocacaoService();
 		Usuario user = new Usuario();
 		Filme film = new Filme();
@@ -29,10 +33,9 @@ public class LocacaoServiceTest {
 		
 		Locacao locacao = ls.alugarFilme(user, film);
 		
-		assertThat(locacao.getValor(), is(CoreMatchers.equalTo(10.5)));
-		assertThat(locacao.getValor(), is(CoreMatchers.not(6.0)));
-		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(locacao.getValor(), is(CoreMatchers.equalTo(10.5)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 		
 	}
 
